d2c768910953fd2c8c7cabc9ad1433560a6bd13f
lanwu1994 <lanwu1994@gmail.com>
2017-02-27 22:40:07 +0200

    jjj

--- mysit/wsgi.py
+++ mysit/wsgi.py
@@ -13,7 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysit.settings")
 
-application = get_wsgi_application()
 
 if "DYNO" in os.environ:
     from dj_static import Cling
