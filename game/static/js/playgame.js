7e8da9369a0b4153bf1f6577a53632781b2837b6
lanwu1994 <lanwu1994@gmail.com>
2017-02-28 18:39:31 +0200

    dhg

---
+++ game/static/js/playgame.js
@@ -0,0 +1,18 @@
+
+window.addEventListener('message',function(e){
+    e.preventDefault();
+    var submitscore=e.data.score;
+    document.getElementById('id_score').value=submitscore;
+    console.log(submitscore);
+
+    $("#play_form").submit(function(ev){
+      $.ajax({
+        type:"POST",
+        url:window.location.href,
+        data: {score:submitscore},
+        success: function(data){console.log("Submit score success, sore is " +submitscore);}
+        error: function(data){console.log("Submit score fail");}
+      });
+
+    });
+},false);
