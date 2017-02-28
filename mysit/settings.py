3340e97ef49b3b7b68b6cb0b894e0f800e4475ee
lanwu1994 <lanwu1994@gmail.com>
2017-02-28 18:53:11 +0200

    dhg

--- mysit/settings.py
+++ mysit/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -122,19 +122,21 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = os.path.join(PROJECT_ROOT,'static')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 #email
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 if "DYNO" in os.environ:
-    STATIC_ROOT = 'collectedfiles'
+
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+    #STATIC_ROOT = 'collectedfiles'
     import dj_database_url
     DATABASES['default'] =  dj_database_url.config()
 
