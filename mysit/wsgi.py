7e224faaad18c241e8731739027ed9052156e50e
lanwu1994 <lanwu1994@gmail.com>
2017-02-28 18:19:44 +0200

    dhg

--- mysit/wsgi.py
+++ mysit/wsgi.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysit.settings")
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysit.settings")
+
 
 
 if "DYNO" in os.environ:
