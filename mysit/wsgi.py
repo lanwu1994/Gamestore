baa5c5d540f97d27e2a9f60c2e20c78d9b937551
lanwu1994 <lanwu1994@gmail.com>
2017-02-28 18:15:03 +0200

    ag

--- mysit/wsgi.py
+++ mysit/wsgi.py
@@ -10,13 +10,14 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysit.settings")
 
 
 if "DYNO" in os.environ:
     from dj_static import Cling
     application = Cling(get_wsgi_application())
+    application = DjangoWhiteNoise(application)
 else:
     application = get_wsgi_application()
         
