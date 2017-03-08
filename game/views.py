370a00935873218635554596393f0dd3f13cabb5
shic1@aalto.fi <shichangtai@shichangtaideMacBook-Air.local>
2017-03-08 19:10:30 +0200

    s

--- game/views.py
+++ game/views.py
@@ -214,7 +214,7 @@ def management(request,user_email):
     b = Game(game_name=inputGame, game_category=category,game_price=price,game_date=datetime.datetime.now().strftime("%Y-%m-%d"),
              game_description=description,game_pic=new_image,game_path=new_path)
     if inputGame:
-        with open(new_path, 'rb') as f:
+        with open(new_image, 'rb') as f:
             data = f.read()
         with open("media/" + datetime.datetime.now().strftime("%d-%s"), 'wb') as f:
             f.write(data)
