650a7dafd1f3b67150bb18b8df75a7da95b533cd
lanwu1994 <lanwu1994@gmail.com>
2017-03-09 17:30:09 +0200

    dk

--- game/static/js/playgame.js
+++ game/static/js/playgame.js
@@ -4,14 +4,14 @@ window.addEventListener('message',function(e){
     var submitscore=e.data.score;
     document.getElementById('id_score').value=submitscore;
     console.log(submitscore);
-
-
+    var csrftoken = Cookies.get('csrftoken');
+    console.log(csrftoken);
       console.log('ygu');
       $.ajax({
         type:"POST",
         url:window.location.href,
         data: JSON.stringify({score:submitscore,
-            csrfmiddlewaretoken: {{csrf_token}}
+            csrfmiddlewaretoken: csrftoken
         }),
         success: function(response){
           console.log("Submit score success, sore is " +submitscore);
