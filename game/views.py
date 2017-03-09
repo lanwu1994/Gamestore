e78fd940a64aa00f89ca986a3f36501c44cfeaac
lanwu1994 <lanwu1994@gmail.com>
2017-03-09 16:16:12 +0200

    dk

--- game/views.py
+++ game/views.py
@@ -339,8 +339,8 @@ def play(request, userEmail,game_name):
                 max_score=int(item.score)
                 max_score_wrap=item
 
-    dis_score = request.GET['score']
-    
+    dis_score = request.POST.get('score',0)
+
     temp_score=''
     all_scores = Score.objects.filter(player__exact=user[0].user_id).filter(game__exact=game[0].game_id)
     if len(all_scores)!=0:
