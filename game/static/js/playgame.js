2cdac5a8e85d95d21300a3277b1ba5a3331adc4d
lanwu1994 <lanwu1994@gmail.com>
2017-03-09 16:31:26 +0200

    dk

--- game/static/js/playgame.js
+++ game/static/js/playgame.js
@@ -10,8 +10,12 @@ window.addEventListener('message',function(e){
         type:"POST",
         url:window.location.href,
         data: JSON.stringify({score:submitscore}),
-        success: function(response){console.log("Submit score success, sore is " +submitscore);}
-        error: function(response){console.log("error");}
+        success: function(response){
+          console.log("Submit score success, sore is " +submitscore);
+        },
+        error: function(response){
+          console.log("error");
+        }
       });
 
     });
