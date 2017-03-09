73dc3548cbd95d79357b0a3c5bda2d59e97fffcc
lanwu1994 <lanwu1994@gmail.com>
2017-03-09 19:03:40 +0200

    dk

--- game/views.py
+++ game/views.py
@@ -341,8 +341,8 @@ def play(request, userEmail,game_name):
                 max_score=int(item.score)
                 max_score_wrap=item
 
-    dis_score = 0
-    dis_score2 = request.body
+    dis_score = 100
+
 
     temp_score=''
     all_scores = Score.objects.filter(player__exact=user[0].user_id).filter(game__exact=game[0].game_id)
@@ -356,7 +356,7 @@ def play(request, userEmail,game_name):
             if dis_score>temp_score.score:
                 temp_score.score=dis_score
                 temp_score.save()
-    return render(request, 'playgame.html',{'user':user[0],'game':game[0],'max_score_wrap':max_score_wrap,'dis_score':dis_score,'yourscore':temp_score,'test':dis_score2})
+    return render(request, 'playgame.html',{'user':user[0],'game':game[0],'max_score_wrap':max_score_wrap,'dis_score':dis_score,'yourscore':temp_score})
 
 def game_edit(request, userEmail,game_name):
     ue=userEmail.split('.com')[0]+'.com'
