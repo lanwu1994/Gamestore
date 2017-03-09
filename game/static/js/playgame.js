c69aa7725f0ceac70c5be1eb43f2232f25cdfb69
lanwu1994 <lanwu1994@gmail.com>
2017-03-09 17:33:27 +0200

    dk

--- game/static/js/playgame.js
+++ game/static/js/playgame.js
@@ -4,9 +4,28 @@ window.addEventListener('message',function(e){
     var submitscore=e.data.score;
     document.getElementById('id_score').value=submitscore;
     console.log(submitscore);
-    var csrftoken = Cookies.get('csrftoken');
-    console.log(csrftoken);
+
       console.log('ygu');
+
+    function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+  var csrftoken = Cookies.get('csrftoken');
+  console.log(csrftoken);
+
+
       $.ajax({
         type:"POST",
         url:window.location.href,
